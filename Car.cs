b3928813a62bdaea507f4d2ef2b523ee2561148b
Alvaro11G <agarcia11746@gmail.com>
2022-09-20 17:08:52 -0600

    initial commit

---
+++ Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car (string makeInput, string modelInput, int year)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = year;
+        }
+
+
+        //make property, model property, and year property
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car MyCar = new Car();
+            MyCar.Make = "Ford";
+            MyCar.Model = "F-150";
+            MyCar.Year = 2018;
+
+            Console.WriteLine($"{MyCar.Make}, {MyCar.Model}, {MyCar.Year}");
+        }
+    }
+}
